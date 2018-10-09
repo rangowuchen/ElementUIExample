9515c803d6bef8073bf8512d646ae5610ce3b633
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2018-10-09 18:42:13 +0800

    补充说明

--- src/pages/ComponentCommunication/components/two.vue
+++ src/pages/ComponentCommunication/components/two.vue
@@ -2,13 +2,15 @@
  * @Author: wuchen
  * @Date: 2018-10-09 15:34:33
  * @LastEditors: wuchen
- * @LastEditTime: 2018-10-09 18:32:33
+ * @LastEditTime: 2018-10-09 18:38:29
  * @Description:这是组件2 
  * @Email: rangowu@163.com
  */
  <template>
     <div>
         <h3>这是组件2</h3>
+        <br>
+        <span>子组件给父组件传值,通过$emit进行传值</span>
         <el-input class="inputLa" v-model="lala"></el-input>
         <el-button type="primary" @click="search">搜索</el-button>
     </div>
