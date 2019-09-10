754cd53860a3658192d75c72fa1739d3abede3ef
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2019-09-10 20:01:22 +0800

    element树单选

---
+++ src/pages/tree/element-tree/singleSelect/index.vue
@@ -0,0 +1,76 @@
+<!--
+ * @Author: wuchen
+ * @Date: 2019-09-10 19:32:14
+ * @LastEditors: wuchen
+ * @LastEditTime: 2019-09-10 19:59:44
+ * @Description: element树单选
+ * @Email: rangowu@163.com
+ -->
+<template>
+    <div>
+        <el-tree :data="treeData" show-checkbox node-key="id" :props="defaultProps" check-strictly @check-change="handleClick" ref="treeRef">
+        </el-tree>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                temp: 0,
+                treeData: [{
+                    id: 1,
+                    label: '一级 1',
+                    children: [{
+                        id: 4,
+                        label: '二级 1-1',
+                        children: [{
+                            id: 9,
+                            label: '三级 1-1-1'
+                        }, {
+                            id: 10,
+                            label: '三级 1-1-2'
+                        }]
+                    }]
+                }, {
+                    id: 2,
+                    label: '一级 2',
+                    children: [{
+                        id: 5,
+                        label: '二级 2-1'
+                    }, {
+                        id: 6,
+                        label: '二级 2-2'
+                    }]
+                }, {
+                    id: 3,
+                    label: '一级 3',
+                    children: [{
+                        id: 7,
+                        label: '二级 3-1'
+                    }, {
+                        id: 8,
+                        label: '二级 3-2'
+                    }]
+                }],
+                defaultProps: {
+                    children: 'children',
+                    label: 'label'
+                }
+            }
+        },
+        methods: {
+            handleClick(data, checked, node) {
+                console.log(data,checked,node)
+                this.temp++;
+                if (this.temp % 2 === 0) {
+                    if (checked) {
+                        this.$refs.treeRef.setCheckedNodes([]);
+                        this.$refs.treeRef.setCheckedNodes([data]);
+                    } else {
+                        this.$refs.treeRef.setCheckedNodes([]);
+                    }
+                }
+            },
+        }
+    }
+</script>
---
+++ src/pages/vue-treeselect/components/disable-branch-nodes.vue
@@ -0,0 +1,79 @@
+<!--
+ * @Author: wuchen
+ * @Date: 2019-09-10 16:28:09
+ * @LastEditors: wuchen
+ * @LastEditTime: 2019-09-10 16:51:43
+ * @Description: 
+ * @Email: rangowu@163.com
+ -->
+<template>
+    <div>
+        <treeselect :multiple="true" :options="options" :disable-branch-nodes="true" v-model="value" search-nested />
+    </div>
+</template>
+
+<script>
+    import Treeselect from "@riophae/vue-treeselect";
+    import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+    export default {
+        components: {
+            Treeselect
+        },
+        data() {
+            return {
+                value: [],
+                options: [{
+                        id: "m",
+                        label: "McDonalds",
+                        children: [{
+                                id: "m-fries",
+                                label: "French Fries"
+                            },
+                            {
+                                id: "m-cheeseburger",
+                                label: "Cheeseburger"
+                            },
+                            {
+                                id: "m-white-cheedar-burger",
+                                label: "White Cheddar Burger"
+                            },
+                            {
+                                id: "m-southwest-buttermilk-crispy-chicken-salad",
+                                label: "Southwest Buttermilk Crispy Chicken Salad"
+                            },
+                            {
+                                id: "m-cola",
+                                label: "Coca-Cola®"
+                            },
+                            {
+                                id: "m-chocolate-shake",
+                                label: "Chocolate Shake"
+                            }
+                        ]
+                    },
+                    {
+                        id: "kfc",
+                        label: "KFC",
+                        children: [{
+                                id: "kfc-fries",
+                                label: "French Fries"
+                            },
+                            {
+                                id: "kfc-chicken-litties-sandwiches",
+                                label: "Chicken Litties Sandwiches"
+                            },
+                            {
+                                id: "kfc-grilled-chicken",
+                                label: "Grilled Chicken"
+                            },
+                            {
+                                id: "kfc-cola",
+                                label: "Pepsi® Cola"
+                            }
+                        ]
+                    }
+                ]
+            };
+        }
+    };
+</script>
