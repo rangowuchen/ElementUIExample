2cc7b58982bd6d1e6756aaffa55b320dfda22b99
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2022-10-10 15:05:50 +0800

    feat:颜色选择优化

--- src/pages/drag/elementPosition.vue
+++ src/pages/drag/elementPosition.vue
@@ -2,15 +2,15 @@
  * @Author: wuchen
  * @Date: 2022-09-28 14:22:49
  * @LastEditors: wuchen
- * @LastEditTime: 2022-09-30 13:58:58
+ * @LastEditTime: 2022-10-10 14:48:47
  * @Description: 
  * @Email: rangowu@163.com
 -->
 <template>
     <div class="contain" ref="contain">
         <div :style="{width: currentX1+'px',height:'100%',background:'darkgray',position:'absolute',left:0,top:0}"></div>
-        <div @mousedown="mousedownL" :style="{width: '4px',height:'24px',background:getColor(currentX1),position:'absolute',left:currentX1+'px',top:'-2px',cursor:'col-resize',zIndex:'99',borderRadius:'2px'}"></div>
-        <div @mousedown="mousedownR" :style="{width: '4px',height:'24px',background:getColor(currentX2),position:'absolute',left:currentX2+'px',top:'-2px',cursor:'col-resize',zIndex:'99',borderRadius:'2px'}"></div>
+        <div @mousedown="mousedownL" :style="{width: '4px',height:'24px',background:getColor(currentX1),position:'absolute',left:currentX1+'px',top:'-2px',cursor:'col-resize',zIndex:'99',borderRadius:'2px'}"><span class="number">{{currentX1}}</span></div>
+        <div @mousedown="mousedownR" :style="{width: '4px',height:'24px',background:getColor(currentX2),position:'absolute',left:currentX2+'px',top:'-2px',cursor:'col-resize',zIndex:'99',borderRadius:'2px'}"><span class="number">{{currentX2}}</span></div>
         <div :style="{width: (containWidth - currentX2)+'px',height:'100%',background:'darkgray',position:'absolute',right:0,top:0}"></div>
     </div>
 </template>
@@ -19,8 +19,8 @@ export default {
     data() {
         return {
             currentX1: 0,// 左侧滑块起始位置
-            currentX2: 200,// 右侧滑块起始位置
-            containWidth: 200,// 容器的宽度
+            currentX2: 100,// 右侧滑块起始位置
+            containWidth: 100,// 容器的宽度
         }
     },
     methods: {
@@ -53,13 +53,13 @@ export default {
             document.onmousemove = function(e2) {
                 e2.preventDefault();
                 // 获取元素移动后与浏览器的距离 - 元素最左边与浏览器之间的距离
-                let bb = e2.clientX - aa;
-                if(bb < 0) {
+                let left = e2.clientX - aa;
+                if(left < 0) {
                     _that.currentX1 = 0;
-                }else if(bb > _that.containWidth) {
+                }else if(left > _that.containWidth) {
                     _that.currentX1 = _that.containWidth;
                 }else {
-                    _that.currentX1 = bb;
+                    _that.currentX1 = left;
                 }
             }
             // 鼠标松开移除事件
@@ -77,14 +77,14 @@ export default {
             document.onmousemove = function(e2) {
                 e2.preventDefault();
                 // 获取元素移动后与浏览器的距离 - 元素最左边与浏览器之间的距离
-                let bb = e2.clientX - aa;
+                let left = e2.clientX - aa;
                 // 右边的滑块不能小于左边的滑块
-                if(bb < _that.currentX1) {
+                if(left < _that.currentX1) {
                     _that.currentX2 = _that.currentX1;
-                }else if(bb > _that.containWidth) {
+                }else if(left > _that.containWidth) {
                     _that.currentX2 = _that.containWidth;
                 }else {
-                    _that.currentX2 = bb;
+                    _that.currentX2 = left;
                 }
             }
             // 鼠标松开移除事件
@@ -100,10 +100,18 @@ export default {
 </script>
 <style lang="scss" scoped>
     .contain {
-        width: 200px;
+        width: 100px;
         height: 20px;
         background: linear-gradient(to right,#3DA57B 0%,#8FC9B4 5%,#E8EC85 10%,#FFDE94 15%,#FFA785 25%,#EC9797 50%,#CF7070 75%,#9E4B4B);
         position: relative;
         margin: 20px 100px;
+        .number {
+            position: absolute;
+            top: -15px;
+            left: 0;
+            color: #666;
+            font-size: 13px;
+            font-weight: 600;
+        }
     }
 </style>
